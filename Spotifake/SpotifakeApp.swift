283ba0ca9f7200e43742249029e6955b366dc040
MauricioChavesDias <59011240+MauricioChavesDias@users.noreply.github.com>
2024-04-23 15:33:00 +1000

    Initial Commit

---
+++ Spotifake/SpotifakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifakeApp.swift
+//  Spotifake
+//
+//  Created by Mauricio Chaves Dias on 23/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
